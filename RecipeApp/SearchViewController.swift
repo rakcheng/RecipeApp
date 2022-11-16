cb20025800a63d6e430a80da07fc803ed7cafec9
Rakery Cheng <rcheng@csumb.edu>
2022-11-15 22:39:13 -0800

    Initial Commit to MenuRec

---
+++ RecipeApp/SearchViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchViewController.swift
+//  RecipeApp
+//
+//  Created by Rak Cheng on 11/15/22.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
